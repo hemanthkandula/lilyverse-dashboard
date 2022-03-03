576e37fca307cee38abde938adcec63cf3d0d42f
Hemanth Kandula <hemanth.kandula@gmail.com>
2022-03-03 05:05:49 -0500

    Remove apps from sidebar

--- src/components/AppLayout/menuStructure.ts
+++ src/components/AppLayout/menuStructure.ts
@@ -1,21 +1,21 @@
-import appsIcon from "@assets/images/menu-apps-icon.svg";
+// import appsIcon from "@assets/images/menu-apps-icon.svg";
 import catalogIcon from "@assets/images/menu-catalog-icon.svg";
 import customerIcon from "@assets/images/menu-customers-icon.svg";
 import discountsIcon from "@assets/images/menu-discounts-icon.svg";
 import homeIcon from "@assets/images/menu-home-icon.svg";
 import ordersIcon from "@assets/images/menu-orders-icon.svg";
-import translationIcon from "@assets/images/menu-translation-icon.svg";
+// import translationIcon from "@assets/images/menu-translation-icon.svg";
 import { commonMessages, sectionNames } from "@saleor/intl";
 import { IntlShape } from "react-intl";
 
-import { appsListPath } from "../../apps/urls";
+// import { appsListPath } from "../../apps/urls";
 import { categoryListUrl } from "../../categories/urls";
 import { collectionListUrl } from "../../collections/urls";
 import { customerListUrl } from "../../customers/urls";
 import { saleListUrl, voucherListUrl } from "../../discounts/urls";
 import { orderDraftListUrl, orderListUrl } from "../../orders/urls";
 import { productListUrl } from "../../products/urls";
-import { languageListUrl } from "../../translations/urls";
+// import { languageListUrl } from "../../translations/urls";
 import { PermissionEnum } from "../../types/globalTypes";
 
 export interface IMenuItem {
@@ -117,22 +117,22 @@ function createMenuStructure(intl: IntlShape): IMenuItem[] {
       permission: PermissionEnum.MANAGE_DISCOUNTS,
       testingContextId: "discounts"
     },
-    {
-      ariaLabel: "apps",
-      icon: appsIcon,
-      label: intl.formatMessage(sectionNames.apps),
-      permission: PermissionEnum.MANAGE_APPS,
-      testingContextId: "apps",
-      url: appsListPath
-    },
-    {
-      ariaLabel: "translations",
-      icon: translationIcon,
-      label: intl.formatMessage(sectionNames.translations),
-      permission: PermissionEnum.MANAGE_TRANSLATIONS,
-      testingContextId: "translations",
-      url: languageListUrl
-    }
+    // {
+    //   ariaLabel: "apps",
+    //   icon: appsIcon,
+    //   label: intl.formatMessage(sectionNames.apps),
+    //   permission: PermissionEnum.MANAGE_APPS,
+    //   testingContextId: "apps",
+    //   url: appsListPath
+    // },
+    // {
+    //   ariaLabel: "translations",
+    //   icon: translationIcon,
+    //   label: intl.formatMessage(sectionNames.translations),
+    //   permission: PermissionEnum.MANAGE_TRANSLATIONS,
+    //   testingContextId: "translations",
+    //   url: languageListUrl
+    // }
   ];
 }
 
